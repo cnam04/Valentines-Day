a39080f8f80ae1d0c0cf0102cf4b8a79a4330ddf
cnam04 <colenam04@gmail.com>
2026-02-13 12:28:20 -0500

    create template for interactive reasons jar.

---
+++ src/components/ReasonsMain.jsx
@@ -0,0 +1,9 @@
+import {useState} from "react";
+import mason from "../assets/mason.png";
+import crumpled from "../assets/crumpled.png";
+import uncrumpled from "../assets/uncrumpled.png";
+
+export default function ReasonsMain(){
+
+}
+
